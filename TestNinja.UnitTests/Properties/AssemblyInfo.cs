bfff6e1f4d0afefc29d69623ae5a25dee75396db
Tony.Krikorian <tony.krikorian@outlook.com>
2019-07-03 20:40:19 -0400

    Primer UnitTesting
    *.rsuser
    *.suo
    *.user
    *.userosscache
    *.sln.docstates
    
    *.userprefs
    
    mono_crash.*
    
    [Dd]ebug/
    [Dd]ebugPublic/
    [Rr]elease/
    [Rr]eleases/
    x64/
    x86/
    [Aa][Rr][Mm]/
    [Aa][Rr][Mm]64/
    bld/
    [Bb]in/
    [Oo]bj/
    [Ll]og/
    
    .vs/
    
    Generated\ Files/
    
    [Tt]est[Rr]esult*/
    [Bb]uild[Ll]og.*
    
    *.VisualState.xml
    TestResult.xml
    nunit-*.xml
    
    [Dd]ebugPS/
    [Rr]eleasePS/
    dlldata.c
    
    BenchmarkDotNet.Artifacts/
    
    project.lock.json
    project.fragment.lock.json
    artifacts/
    
    StyleCopReport.xml
    
    *_i.c
    *_p.c
    *_h.h
    *.ilk
    *.meta
    *.obj
    *.iobj
    *.pch
    *.pdb
    *.ipdb
    *.pgc
    *.pgd
    *.rsp
    *.sbr
    *.tlb
    *.tli
    *.tlh
    *.tmp
    *.tmp_proj
    *_wpftmp.csproj
    *.log
    *.vspscc
    *.vssscc
    .builds
    *.pidb
    *.svclog
    *.scc
    
    _Chutzpah*
    
    ipch/
    *.aps
    *.ncb
    *.opendb
    *.opensdf
    *.sdf
    *.cachefile
    *.VC.db
    *.VC.VC.opendb
    
    *.psess
    *.vsp
    *.vspx
    *.sap
    
    *.e2e
    
    $tf/
    
    *.gpState
    
    _ReSharper*/
    *.[Rr]e[Ss]harper
    *.DotSettings.user
    
    .JustCode
    
    _TeamCity*
    
    *.dotCover
    
    .axoCover/*
    !.axoCover/settings.json
    
    *.coverage
    *.coveragexml
    
    _NCrunch_*
    .*crunch*.local.xml
    nCrunchTemp_*
    
    *.mm.*
    AutoTest.Net/
    
    .sass-cache/
    
    [Ee]xpress/
    
    DocProject/buildhelp/
    DocProject/Help/*.HxT
    DocProject/Help/*.HxC
    DocProject/Help/*.hhc
    DocProject/Help/*.hhk
    DocProject/Help/*.hhp
    DocProject/Help/Html2
    DocProject/Help/html
    
    publish/
    
    *.[Pp]ublish.xml
    *.azurePubxml
    *.pubxml
    *.publishproj
    
    PublishScripts/
    
    *.nupkg
    *.snupkg
    **/[Pp]ackages/*
    !**/[Pp]ackages/build/
    *.nuget.props
    *.nuget.targets
    
    csx/
    *.build.csdef
    
    ecf/
    rcf/
    
    AppPackages/
    BundleArtifacts/
    Package.StoreAssociation.xml
    _pkginfo.txt
    *.appx
    *.appxbundle
    *.appxupload
    
    *.[Cc]ache
    !?*.[Cc]ache/
    
    ClientBin/
    ~$*
    *~
    *.dbmdl
    *.dbproj.schemaview
    *.jfm
    *.pfx
    *.publishsettings
    orleans.codegen.cs
    
    Generated_Code/
    
    _UpgradeReport_Files/
    Backup*/
    UpgradeLog*.XML
    UpgradeLog*.htm
    ServiceFabricBackup/
    *.rptproj.bak
    
    *.mdf
    *.ldf
    *.ndf
    
    *.rdl.data
    *.bim.layout
    *.bim_*.settings
    *.rptproj.rsuser
    *- [Bb]ackup.rdl
    *- [Bb]ackup ([0-9]).rdl
    *- [Bb]ackup ([0-9][0-9]).rdl
    
    FakesAssemblies/
    
    *.GhostDoc.xml
    
    .ntvs_analysis.dat
    node_modules/
    
    *.plg
    
    *.opt
    
    *.vbw
    
    **/*.HTMLClient/GeneratedArtifacts
    **/*.DesktopClient/GeneratedArtifacts
    **/*.DesktopClient/ModelManifest.xml
    **/*.Server/GeneratedArtifacts
    **/*.Server/ModelManifest.xml
    _Pvt_Extensions
    
    .paket/paket.exe
    paket-files/
    
    .fake/
    
    .cr/personal
    
    __pycache__/
    *.pyc
    
    *.tss
    
    *.jmconfig
    
    *.btp.cs
    *.btm.cs
    *.odx.cs
    *.xsd.cs
    
    OpenCover/
    
    ASALocalRun/
    
    *.binlog
    
    *.nvuser
    
    .mfractor/
    
    .localhistory/
    
    healthchecksdb
    
    MigrationBackup/

---
+++ TestNinja.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestNinja.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b8bbf930-7a7a-4f78-b74b-aab900728e3e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        //NombreMetodo_Escenario_ResultadoEsperado
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+
+            //Act
+           var result = reservation.CanBeCancelledBy(new User() { IsAdmin = true });
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
