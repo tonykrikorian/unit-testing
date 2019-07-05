4f749946448692ce7a1aeab8bca2b6d9e8a9715d
Tony.Krikorian <tony.krikorian@outlook.com>
2019-07-04 22:29:13 -0400

    Math Class Tests

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,59 @@
+﻿
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            //Actual
+            var result = _math.Add(1,2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnFirstArgument()
+        {
+
+            //Actual
+            var result = _math.Max(2, 1);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnSecondArgument()
+        {
+            //Actual
+            var result = _math.Max(1, 2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnSameArgument()
+        {
+            //Actual
+            var result = _math.Max(1, 1);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(1));
+        }
+    }
+}
