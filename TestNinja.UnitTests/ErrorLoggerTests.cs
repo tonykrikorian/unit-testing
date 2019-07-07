14fd4e4e150056865383a5a6ad579a98d6287ff8
Tony.Krikorian <tony.krikorian@outlook.com>
2019-07-07 17:47:50 -0400

    Testing Methods Return void Change Object State

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("dd");
+
+            Assert.That(logger.LastError, Is.EqualTo("dd").IgnoreCase);
+
+        }
+    }
+}
