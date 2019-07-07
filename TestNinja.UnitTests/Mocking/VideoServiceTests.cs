163effd2500127efaaa6f293b75f21ce8f16fdf1
Tony.Krikorian <tony.krikorian@outlook.com>
2019-07-07 19:40:19 -0400

    Refactoring Loosely Coupled Code
    Refactoriza el codigo para desacoplarlo mediante interfaces para usar
    servicios externos Fake

---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class VideoServiceTests
+    {
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnErrorMessage()
+        {
+            var service = new VideoService();
+
+            var result = service.ReadVideoTitle(new FakeFileReader());
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
--- TestNinja/Mocking/VideoService.cs
+++ TestNinja/Mocking/VideoService.cs
@@ -9,9 +9,11 @@ namespace TestNinja.Mocking
 {
     public class VideoService
     {
-        public string ReadVideoTitle()
+        public string ReadVideoTitle(IFileReader fileReader)
         {
-            var str = File.ReadAllText("video.txt");
+            //var str = File.ReadAllText("video.txt");
+            //var str = new FileReader().Read("video.txt");
+            var str = fileReader.Read("video.txt");
             var video = JsonConvert.DeserializeObject<Video>(str);
             if (video == null)
                 return "Error parsing the video.";
